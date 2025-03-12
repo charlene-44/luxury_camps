027658d6cd3a8c662b50c9702678822412a7fb56
Ange230700 <kouakouangeericstephane@gmail.com>
2025-03-12 20:21:44 +0100

    ok2

--- backend/src/main/java/com/camps/backend/repositories/FurnitureRepository.java
+++ backend/src/main/java/com/camps/backend/repositories/FurnitureRepository.java
@@ -3,7 +3,6 @@
 package com.camps.backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.camps.backend.models.Furniture;
 
