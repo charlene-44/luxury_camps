b8beccac2609d1c9a4f9d3177a58162ddba590f8
CharlotteBCK <charlotte44300@gmail.com>
2025-03-18 16:39:43 +0100

    ajout furnitures

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -99,13 +99,26 @@ CREATE TABLE `Image` (
 -- Insert furniture types first
 INSERT INTO FurnitureType (name) VALUES 
 ('Sofa'),
-('Table'),
-('Cousin');
+('Chaise'),
+('Coussin'),
+('Plateau'),
+('Chaise'),
+('Lampe'),
+('Videpoche'),
+('Cabriolet'),
+('Cendrier'),
+('Sculpture'),
+('Secrétaire'),
+('Table d''appoint'),
+('Fauteuil'),
+('Canapé');
 
 -- Insert materials
 INSERT INTO Material (name) VALUES 
-('Leather'),
-('Wood');
+('Cuir'),
+('Bois'),
+('Tissus'),
+('Porcelaine');
 
 -- Insert furniture records
 INSERT INTO Furniture (
@@ -119,42 +132,111 @@ INSERT INTO Furniture (
     status
 ) VALUES 
 (
-    'Canapé Dior',
-    'A comfortable modern leather sofa',
-    1,
-    'Large',
-    'Nude',
+    'Canapé Louis Vuitton',
+    'Issu de la collection Objets Nomades Louis Vuitton, le canapé Bomboca des frères Campana évoque une confiserie brésilienne. Sa base courbée en cuir et ses huit coussins emboîtés allient confort et esthétisme.',
+    14,
+    '182 x 88 x 112 cm',
+    'Beige',
     10,
-    79999.99,
+    60000.00,
     'Available'
 ),
+
 (
     'Table Hermes',
-    'Elegant wooden table',
-    2,
-    'Medium',
-    'Blue white',
+    'Table d appoint avec plateau en porcelaine decoree a la main. Pietement pliant en chene massif, sangles en cuir a bride. Dessin « H Infini » d Henri d Origny.',
+    12,
+    'D 34 x H 51 cm',
+    'bleu / blanc',
     5,
-    2499.99,
+    5700,
     'Available'
 ),
 (
     'Cabriolet Hermes',
-    'Elegant chair',
-    3,
-    'H60 P40 L60',
-    'Black',
+    'Cabriolet avec assise compacte, pietement en noyer Canaletto massif. Coque, assise et dossier gainees de cuir taurillon H. Petit fauteuil d appoint confortable.',
+    8,
+    'L 68,5 x H 72,5 x P 64,5 cm',
+    'Noir',
     2,
-    3399.99,
+    15700,
+    'Available'
+),
+(
+    'Chaise Dior',
+    'Chaise médaillon en bois de hêtre à motif Toile de Jouy bleu fabriquée par le fournisseur historique du château de Versailles.',
+    2,
+    '100 x 48 x 44 cm',
+    'Bleu',
+    4,
+    35000,
+    'Available'
+),
+(
+    'Lampe Hermes',
+    'Lampe de table gainée de taurillon lisse. Abat-jour en opaline soutenu par 3 branches de cuir. Socle détourné en vide-poches. Variateur. Éclairage diffus',
+    6,
+    'L 32 x H 70 cm ',
+    'Blanc',
+    25,
+    9200,
+    'Available'
+),
+(
+    'Chaise Louis Vuitton',
+    'Chaise Swing par Patricia Urquiola, édition limitée. Allure moderne et savoir-faire artisanal. Mesh tissé à la main, bordure à franges ludiques. Nœuds inspirés du motif Monogram et poignées en cuir.',
+    2,
+    '130 x 200 x 60 cm',
+    'Blanc/Marron',
+    1,
+    315000,
+    'Available'
+),
+(
+    'Plateau Dior',
+    'Collection Pierre Yovanovitch pour Dior Maison, inspirée des châteaux de la Colle Noire et de Fabrègues. 15 objets alliant simplicité, solidité et noblesse, mettant en valeur savoir-faire traditionnels et matériaux locaux',
+    4,
+    '130 x 200 x 60 cm',
+    'Bleu/Noir',
+    22,
+    1500,
+    'Available'
+),
+(
+    'Secrétaire Hermes',
+    'Secrétaire et petite table de travail en chêne naturel, avec caisson gainé de taurillon H. Compartiment passe-câbles, deux casiers amovibles et tiroirs avec poignées en cuir. L''ensemble modulable selon l''envie.',
+    11,
+    'L 85 x H 72,2 x P 52 cm',
+    'Marron',
+    3,
+    39800,
+    'Available'
+),
+(
+    'Vide Poche Dior',
+    'Vide poche en porcelaine de Limoges extra-fine au motif Muguet (Lily of the Valley) vert. Le muguet est l''un des porte-bonheur de Monsieur Dior qui portait toujours un brin dans sa poche lors de ses défilés.',
+    7,
+    '20 x 16 cm',
+    'Vert',
+    100,
+    410,
     'Available'
 );
+
+
+
+
 
 -- Link furniture to materials
 INSERT INTO Furniture_Material (id_furniture, id_material) VALUES 
 (1, 1),
-(1, 2),
 (2, 2),
-(3,1);
+(3, 1),
+(4, 2),
+(5, 3),
+(6, 3),
+(7, 2),
+(8, 4);
 
 -- Insert images (après avoir créé les meubles)
 INSERT INTO Image (id_furniture, url) VALUES 
@@ -169,4 +251,36 @@ INSERT INTO Image (id_furniture, url) VALUES
 (2, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Hermes/table_appoint_detail_3.png'),
 
 -- Image pour le Cabriolet Hermes (id_furniture = 3)
-(3, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Hermes/Cabriolet.webp');
+(3, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Hermes/Cabriolet.webp'),
+(3, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Hermes/Cabriolet_detail2.webp'),
+(3, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Hermes/Cabriolet_detail3.webp'),
+
+-- Image pour le chaise Dior (id_furniture = 4)
+(4, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Dior/chaise.webp'),
+(4, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Dior/chaise_detail1.webp'),
+(4, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Dior/chaise_detail3.webp'),
+
+-- Image pour la lampe Hermes (id_furniture = 5)
+(5, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Hermes/Lampe.webp'),
+(5, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Hermes/Lampe_detail1.webp'),
+(5, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Hermes/Lampe_detail2.webp'),
+
+-- Image pour le chaise LV (id_furniture = 6)
+(6, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/LV/chaise_1.avif'),
+(6, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/LV/chaise_detail1.avif'),
+(6, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/LV/chaise_detail2.avif'),
+
+-- Image pour le plateau Dior (id_furniture = 7)
+(7, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Dior/Plateau.webp'),
+(7, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Dior/Plateau_detail1.webp'),
+(7, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Dior/Plateau_detail3.webp'),
+
+-- Image pour le secrétaire Hermes (id_furniture = 8)
+(8, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Hermes/secretaire.webp'),
+(8, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Hermes/secretaire.webp'),
+(8, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Hermes/secretaire.webp'),
+
+-- Image pour le plateau Dior (id_furniture = 9)
+(9, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Dior/videpoche.webp'),
+(9, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Dior/videpoche_detail1.webp'),
+(9, 'https://raw.githubusercontent.com/charlene-44/luxury_camps/dev/frontend/public/assets/Dior/videpoche_detail2.webp');
