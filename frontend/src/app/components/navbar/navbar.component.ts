97efdd904d843ce79effa89f84d60f6e85d67ae6
Ange230700 <kouakouangeericstephane@gmail.com>
2025-03-18 20:16:04 +0100

    donne du style à la navbar

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,26 +1,14 @@
 // frontend\src\app\components\navbar\navbar.component.ts
 
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, MenubarModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-  constructor(private readonly router: Router) {}
-
-  gotoHomepage() {
-    this.router.navigate(['/']);
-  }
-
-  gotoAdminPage() {
-    this.router.navigate(['/furniture-management']);
-  }
-  
-  gotoFurnitureCreationPage() {
-    this.router.navigate(['/furniture-add']);
-  }
-}
+export class NavbarComponent {}
